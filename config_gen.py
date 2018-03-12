a39afc851769e884931049e38fa33cc377a48b3f
xltan <lidmuse@gmail.com>
2018-03-12 17:40:56 +0800

    first commit

--- config_gen.py
+++ config_gen.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
 import os
 import tempfile
@@ -17,69 +16,38 @@ def main():
         return 1
     print('Found CMakeLists.txt in "'+cmake_path+'"')
 
-    # Refuse to overwrite unless explicitely asked to
-    if os.path.exists(os.path.join(cmake_path, CONFIG_FILE_NAME)) and not '-f' in sys.argv[1:]:
-        print("A YouCompleteMe config file already exists in \""+cmake_path+"\", giving up.")
-        return 1
-
     tmpdir = tempfile.mkdtemp()
     ret = genConf(tmpdir, cmake_path)
+    dbpath = 'compile_commands.json'
+    shutil.copyfile(os.path.join(tmpdir, dbpath), os.path.join(cmake_path, dbpath))
+    confpath = os.path.join(os.path.dirname(__file__), CONFIG_FUNCTIONS_FILE)
+    shutil.copyfile(confpath, os.path.join(cmake_path, CONFIG_FILE_NAME))
     shutil.rmtree(tmpdir, ignore_errors=True)
     return ret
 
+def source_bat(bat_file):
+    # interesting = {"INCLUDE", "LIB", "LIBPATH", "PATH"}
+    result = {}
+
+    process = subprocess.Popen('"%s"& set' % (bat_file),
+                        stdout=subprocess.PIPE,
+                        shell=True)
+    (out, err) = process.communicate()
+
+    for line in out.split("\n"):
+        if '=' not in line:
+            continue
+        line = line.strip()
+        key, value = line.split('=', 1)
+        result[key] = value
+    return result
+
 def genConf(tmpdir, cmake_path): 
-    with open(os.path.dirname(__file__)+'/'+CONFIG_FUNCTIONS_FILE, 'r') as file:
-        config_functions = file.read()
     os.chdir(tmpdir)
-    if runCMake(tmpdir, cmake_path) != 0:
-        print("Failed to run CMake, couldn't generate YCM config");
-        return 1
-
-    # Find the project name from the cache
-    with open('CMakeCache.txt', 'r') as file:
-        cache_data = file.read()
-    pos = cache_data.index('CMAKE_PROJECT_NAME:STATIC')
-    if pos == -1:
-        print("Failed to parse CMake cache")
-        return -1
-    project_name = cache_data[pos+len('CMAKE_PROJECT_NAME:STATIC')+1:]
-    pos = project_name.find('\n')
-    if pos != -1:
-        project_name = project_name[:pos]
-
-    # Find the flags from the project.dir
-    flags = []
-    includes = []
-    with open('CMakeFiles/'+project_name+'.dir/flags.make', 'r') as file:
-        flags_data = filter(None, file.read().split('\n'))
-    for line in flags_data:
-        if (line.startswith('C_FLAGS') or line.startswith('CXX_FLAGS')) and line.find('=') != -1:
-            flags.extend(line[line.find('=')+1:].strip().split(' '))
-        if (line.startswith('C_INCLUDES') or line.startswith('CXX_INCLUDES')) and line.find('=') != -1:
-            includes.extend(line[line.find('=')+1:].strip().split(' '))
-
-    # Write the config
-    writeConf(cmake_path, flags, includes, config_functions)
-    print("YouCompleteMe configuration generated successfully!")
-
-# Writes the YCM config file
-def writeConf(path, flags, includes, config_functions):
-    with open(path+'/'+CONFIG_FILE_NAME, 'w') as conf_file:
-        conf_file.write("import os,ycm_core\n")
-
-        conf_file.write("flags = [\n")
-        for flag in flags:
-            conf_file.write("'"+flag+"',\n")
-        for include in includes:
-            conf_file.write("'"+include+"',\n")
-        conf_file.write("]\n")
-
-        conf_file.write(config_functions)
-
-# Runs CMake for a project in the given temporary directory
-def runCMake(tmpdir, project_dir):
     print("Running cmake in \""+tmpdir+"\"...")
-    return subprocess.call(["cmake", project_dir], cwd=tmpdir, stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
+    res = source_bat('C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat')
+    os.environ.update(res)
+    return subprocess.call(["cmake", "-G", "NMake Makefiles", "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1", cmake_path], cwd=tmpdir, stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
 
 # Goes up as needed in the directory hierarchy until a given file is found, or the root is reached
 def findParentFile(filename, base_path=None):
--- plugin/ycmgen.vim
+++ plugin/ycmgen.vim
@@ -1,12 +1,8 @@
 let s:config_gen = expand("<sfile>:p:h:h") . "/config_gen.py"
 
-command! -nargs=0 -complete=file_in_path -bang YcmGen call s:GenerateConfig(<bang>0)
+command! -nargs=0 -complete=file_in_path -bang YcmGen call s:GenerateConfig()
 
-function! s:GenerateConfig(overwrite)
-    let l:cmd = "! " . s:config_gen
-    if a:overwrite
-        let l:cmd = l:cmd . " -f"
-	endif
-
-    execute l:cmd . " </dev/null"
+function! s:GenerateConfig()
+    let l:cmd = "silent !python " . s:config_gen
+    execute l:cmd . " -f"
 endfunction()
